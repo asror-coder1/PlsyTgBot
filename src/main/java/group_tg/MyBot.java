fa97f3c69958f234a1c92e4125e1384fa874fec4
asror-coder1 <afayzullayev499@gmail.com>
2025-12-15 16:30:31 +0500

    add commitproject

--- src/main/java/group_tg/MyBot.java
+++ src/main/java/group_tg/MyBot.java
@@ -1,10 +1,14 @@
 package group_tg;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class MyBot extends TelegramLongPollingBot {
+    MyBotService myBotService = new MyBotService();
     @Override
     public void onUpdateReceived(Update update) {
 
@@ -20,10 +24,46 @@ public class MyBot extends TelegramLongPollingBot {
             System.out.println("Firstname:" + firstName + "\nLastname:"+
                     lastName + "\nText:" + text + "\nchatId:" + chatId);
 
+//            Reply button execute qilib chaqirish
+
+
+
+
         } else if (update.hasCallbackQuery()) {
             CallbackQuery callbackQuery = update.getCallbackQuery();
+            String data = callbackQuery.getData();
             Long chatId = callbackQuery.getMessage().getChatId();
             Integer messageId = update.getMessage().getMessageId();
+
+
+//            if (data.equals("nimadurId")){
+//                DeleteMessage deleteMessage = new DeleteMessage();
+//                deleteMessage.setMessageId(messageId);
+//                deleteMessage.setChatId(chatId);
+//
+//                try {
+//                    execute(myBotService.start(chatId));
+//                } catch (TelegramApiException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+
+//
+//            if (data.equals("nimadurId")){
+//                EditMessageReplyMarkup editMessageReplyMarkup = new EditMessageReplyMarkup();
+//                editMessageReplyMarkup.setMessageId(messageId);
+//                editMessageReplyMarkup.setChatId(chatId);
+//                editMessageReplyMarkup.setReplyMarkup(myBotService.(chatId));
+//
+//                try {
+//                    execute(editMessageReplyMarkup);
+//                } catch (TelegramApiException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+
+
+
         }
 
     }
--- src/main/java/group_tg/MyBotService.java
+++ src/main/java/group_tg/MyBotService.java
@@ -1,4 +1,15 @@
 package group_tg;
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
 public class MyBotService {
+
+//    start bosilganda chiqadigan button va text
+
+    public SendMessage start(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Assalomu aleykum botimizga xush kelibsiz");
+        return sendMessage;
+    }
 }
